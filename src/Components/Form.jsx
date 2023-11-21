f353cfecaa077ba7b6863c5723b01d19b3542e93
juaniaraujoc <juaniaraujocroizier@gmail.com>
2023-11-21 19:39:52 -0300

    por entregar

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = ({ email, setEmail }) => {
-  // Validación de email
+const Form = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
   const isValidEmail = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && email.endsWith('.com'); 
-    //utilizo una expresión regular
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && email.endsWith(".com");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (fullName.length > 5 && isValidEmail(email)) {
+      // Simulamos el envío del formulario (en este caso, solo mostramos los datos por consola)
+      console.log("Datos del formulario:", { fullName, email });
+
+      // Mostramos el mensaje de éxito
+      setIsFormSubmitted(true);
+    } else {
+      // Mostramos el mensaje de error
+      console.error("Por favor, verifique su información nuevamente");
+    }
   };
 
   return (
     <div>
-      <form>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        {email && !isValidEmail(email) && (
-          <p style={{ color: 'red' }}>Por favor, ingrese un email válido</p>
-        )}
-        {/* Otros elementos del formulario */}
-      </form>
+      {isFormSubmitted ? (
+        <p>
+          Thank you, {fullName}. We will contact you as soon as possible via email.
+        </p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="fullName">Full Name:</label>
+            <input
+              type="text"
+              id="fullName"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {fullName.length > 5 && !isValidEmail(email) && (
+            <p style={{ color: "red" }}>Please enter a valid email</p>
+          )}
+          <button type="submit">Enviar</button>
+        </form>
+      )}
     </div>
   );
 };
